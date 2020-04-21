5db98d6736e60579313650706235b9f9818dd2ef
Marco De Clara <marcodeclara@gmail.com>
2020-04-21 10:33:59 +0200

    Code optimized

--- main.js
+++ main.js
@@ -5,22 +5,28 @@
 
 var distance, age, ticketPrice;
 
+//cost for each kilometres
+var priceKm = 0.21;
+
+// age discount
+var underageDisc = 0.8;
+var overDisc = 0.6;
+
 // ask travel distance in km
 distance = prompt("Please enter your travel distance: ", "using kilometres");
 
 // default ticket ticket price
-ticketPrice = distance * 0.21;
+ticketPrice = distance * priceKm;
 
 // ask customer's age
 age = prompt("Please enter your age: ", "using digits");
 
-// age discount
 // underage discount: 20%
 if ( age < 18 ) {
-    ticketPrice = ticketPrice * 0.8;
+    ticketPrice = ticketPrice * underageDisc;
     //over 65 discount: 40%
 } else if ( age > 65 ) {
-        ticketPrice = ticketPrice * 0.6;
+        ticketPrice = ticketPrice * overDisc;
     }
 
 // price round to two decimal places
